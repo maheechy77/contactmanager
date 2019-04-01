cade96e27b57d8258d3a42ce9ee3b20d23073c55
Mahi <maheechy96@gmail.com>
2019-04-02 02:25:02 +0600

    Updates

---
+++ precache-manifest.3f7ccde8ae64a89ecf1f755462a54de6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "727adb7d97db37f9bcf6",
+    "url": "/contactmanager/static/js/main.9caeffc9.chunk.js"
+  },
+  {
+    "revision": "f4470bbe318ff4dd055b",
+    "url": "/contactmanager/static/js/2.7f5b4121.chunk.js"
+  },
+  {
+    "revision": "727adb7d97db37f9bcf6",
+    "url": "/contactmanager/static/css/main.1c8e59ae.chunk.css"
+  },
+  {
+    "revision": "f4470bbe318ff4dd055b",
+    "url": "/contactmanager/static/css/2.8fd55820.chunk.css"
+  },
+  {
+    "revision": "306bc2d0088dbfc1dfcab34355e9409f",
+    "url": "/contactmanager/index.html"
+  }
+];
